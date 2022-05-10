b8f2d50547dbbec2ba0997d6fd2d9f024cae31e0
Zak McRae <zakmcrae@gmail.com>
2022-05-10 14:32:29 -0400

    setup eslint and plugins
    react, react hooks and a11y plugins added

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/strict",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
