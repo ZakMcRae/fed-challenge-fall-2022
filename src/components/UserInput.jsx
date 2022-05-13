eaa2623297a2553493e3f8c27ada7fce01d40c31
Zak McRae <zakmcrae@gmail.com>
2022-05-13 10:25:39 -0400

    user input component
    placeholder getResponse function for now
    still need to add API call

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+
+const UserInput = ({ responses, setResponses }) => {
+  // get API response using user input prompt
+  const getResponse = (e) => {
+    e.preventDefault();
+    const prompt = document.querySelector("#prompt");
+
+    //todo - make actual API call
+
+    setResponses([
+      ...responses,
+      {
+        prompt: prompt.value,
+        response:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nunc enim, accumsan quis est sit amet, dictum pulvinar sem. Suspendisse potenti. Praesent quis velit vitae ex pulvinar pharetra quis quis diam. Curabitur sit amet sagittis nibh, in lobortis tortor. In hac habitasse platea dictumst. Suspendisse sollicitudin bibendum eleifend. Phasellus elementum neque vel faucibus venenatis.",
+        id: responses.length,
+      },
+    ]);
+
+    prompt.value = null;
+  };
+
+  return (
+    <div className="user-input-section">
+      <p className="justify-start">Enter Prompt</p>
+      <textarea name="promptInput" id="prompt" rows="15"></textarea>
+      <button onClick={getResponse}>Submit</button>
+    </div>
+  );
+};
+
+UserInput.propTypes = {
+  responses: PropTypes.arrayOf(
+    PropTypes.shape({
+      prompt: PropTypes.string,
+      response: PropTypes.string,
+    })
+  ),
+  setResponses: PropTypes.func,
+};
+
+export default UserInput;
