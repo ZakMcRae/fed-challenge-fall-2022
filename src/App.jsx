b13c72aa68edfdb360b541cda48d86a5708fbe90
Zak McRae <zakmcrae@gmail.com>
2022-05-13 10:23:26 -0400

    setup basic app structure
    store state in app componenet to pass down to other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import Responses from "./components/Responses";
+import UserInput from "./components/UserInput";
 
 function App() {
+  const [responses, setResponses] = useState([]);
+
   return (
     <div className="App">
-      <p>App</p>
+      <h1 className="page-title justify-start">Fun with AI</h1>
+      <UserInput responses={responses} setResponses={setResponses} />
+      {responses.length > 0 ? <Responses responses={responses} /> : null}
     </div>
   );
 }
