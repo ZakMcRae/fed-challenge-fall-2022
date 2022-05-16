654e15d16e174fb6c93c4525269089ffd0ba58e9
Zak McRae <zakmcrae@gmail.com>
2022-05-16 09:56:18 -0400

    added example config file
    config file w/ instructions for handling api key to keep out of git repo

---
+++ example-config.js
@@ -0,0 +1,5 @@
+// rename this file to config.js and fill in your OpenAI api key
+
+// split the OpenAI api key into 2 parts to prevent the api key from getting scraped on github
+export const API_KEY1 = "";
+export const API_KEY2 = "";
